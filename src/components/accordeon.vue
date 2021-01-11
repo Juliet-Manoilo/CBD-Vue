d157c3465f4248f3b7b32d415735caff79d5752c
juliett manoilo <mnl.juliett@gmail.com>
2021-01-11 18:53:36 +0200

    :construction: tring to make an accordion:

--- src/components/accordeon.vue
+++ src/components/accordeon.vue
@@ -4,6 +4,7 @@
 <section class="faq">
         <h2 class="faq__title">Frequently asked questions</h2>
 <div class="faq__accordeon">
+  
 </div>
 </section>
 </template>
@@ -20,7 +21,13 @@ export default {
 
 <style lang="scss">
 @import "../styles/core/vars.scss";
+.faq {
+  font-family: $main-font;
+  min-height: 50vh;
+  &__title {
+    text-transform: uppercase;
+  }
+}
 
-  
 
 </style>
